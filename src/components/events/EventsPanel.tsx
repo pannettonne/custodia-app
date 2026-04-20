c4da5acbea68284e540585b2f5303373da533c38
pannettonne <96749943+pannettonne@users.noreply.github.com>
2026-04-20 22:42:30 +0200

    fix build: add typed events panel wrapper

--- src/components/events/EventsPanel.tsx
+++ src/components/events/EventsPanel.tsx
@@ -1,2 +1,13 @@
 'use client'
-export { EventsPanel } from './EventsPanelWithLocation'
+import { EventsPanel as EventsPanelImpl } from './EventsPanelWithLocation'
+
+type EventsPanelProps = {
+  focusTargetId?: string
+  focusSeq?: number
+  initialCreateDate?: string
+  createSeq?: number
+}
+
+export function EventsPanel(props: EventsPanelProps) {
+  return <EventsPanelImpl {...props} />
+}
