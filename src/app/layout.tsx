c92f066ad50bf61139b868d4f0ed3b0797f9c7ff
pannettonne <96749943+pannettonne@users.noreply.github.com>
2026-05-02 16:08:38 +0200

    Revert More menu grouping import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata, Viewport } from 'next'
 import './globals.css'
-import './more-menu.css'
 import { AuthProvider } from '@/lib/auth-context'
 import { ThemeProvider } from '@/lib/theme-context'
 
